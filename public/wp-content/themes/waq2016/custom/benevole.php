7e83d76e29d3c10bec83b5de14d4607f2a37878d
Paul Côté <github@paulcote.net>
2016-01-12 17:08:27 -0500

    Added linked author to benevole

--- public/wp-content/themes/waq2016/custom/benevole.php
+++ public/wp-content/themes/waq2016/custom/benevole.php
@@ -27,5 +27,76 @@ function waq2016_add_benevole_post_type() {
 
 if(function_exists("register_field_group"))
 {
+    register_field_group(array (
+		'id' => 'acf_linked_author',
+		'title' => 'Auteur',
+		'fields' => array (
+			array (
+				'key' => 'field_56956a6f5242c',
+				'label' => 'Auteur lié',
+				'name' => 'linked_author',
+				'type' => 'user',
+				'role' => array (
+					0 => 'all',
+				),
+				'field_type' => 'select',
+				'allow_null' => 0,
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'benevole',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'side',
+			'layout' => 'no_box',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
 
+	register_field_group(array (
+		'id' => 'acf_informations',
+		'title' => 'Informations',
+		'fields' => array (
+			array (
+				'key' => 'field_56956b5140287',
+				'label' => 'Comité',
+				'name' => 'committee',
+				'type' => 'text',
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'benevole',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'normal',
+			'layout' => 'no_box',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
 }
