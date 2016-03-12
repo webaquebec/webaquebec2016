dd0c7b0076b7f6a92cb478afee412b055f03016d
Paul Côté <github@paulcote.net>
2016-03-11 21:36:44 -0500

    Fixed saving of data device

--- public/wp-content/themes/waq2016/api/api-devices.php
+++ public/wp-content/themes/waq2016/api/api-devices.php
@@ -19,17 +19,17 @@ if (isset($postArgs['ID'])) {
 } else {
     $id = wp_insert_post($postArgs);
 }
-var_dump('location');
-var_dump($device->location);
 
 if(isset($device->location)){
+var_dump('location');
+var_dump($device->location);
     update_post_meta($id, 'location', ($device->location ? $device->location : ""));
 }
 
-var_dump('schedule');
-var_dump($device->schedule);
 
 if(isset($device->schedule)){
+var_dump('schedule');
+var_dump($device->schedule);
     update_post_meta($id, 'schedule', ($device->schedule ? $device->schedule : ""));
 }
 
