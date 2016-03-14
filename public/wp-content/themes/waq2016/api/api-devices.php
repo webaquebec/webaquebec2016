871f1824b6da8478c8998ed218c5e241d4116a30
Paul Côté <github@paulcote.net>
2016-03-13 20:24:52 -0400

    Fixed saving of data device

--- public/wp-content/themes/waq2016/api/api-devices.php
+++ public/wp-content/themes/waq2016/api/api-devices.php
@@ -1,6 +1,7 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
   exit;
 }
 
