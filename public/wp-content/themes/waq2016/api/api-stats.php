9bdfbcda423b319979fa0a942c85b54f1b06f056
Paul Côté <github@paulcote.net>
2016-04-06 17:31:57 -0400

    Added precision to stats

--- public/wp-content/themes/waq2016/api/api-stats.php
+++ public/wp-content/themes/waq2016/api/api-stats.php
@@ -18,6 +18,12 @@ function waq2016_get_devices_count_for_schedule($scheduleId){
 function waq2016_get_devices_count_for_room($roomId){
     global $wpdb;
 
+    $currentTime = time();
+    $fifteen = $currentTime - (15*60*1000);
+    $thirty = $currentTime - (30*60*1000);
+    $fortyfive = $currentTime - (45*60*1000);
+    $hour = $currentTime - (60*60*1000);
+
     $querystr = "
     SELECT wposts.*
     FROM $wpdb->posts wposts, $wpdb->postmeta wpostmeta
@@ -27,7 +33,57 @@ function waq2016_get_devices_count_for_room($roomId){
     AND wposts.post_type = 'device'
     AND wposts.post_status = 'publish'
     ";
-    return count($wpdb->get_results($querystr));
+    $allCount = count($wpdb->get_results($querystr));
+
+    $querystr = "
+    SELECT wposts.*
+    FROM $wpdb->posts wposts, $wpdb->postmeta wpostmeta
+
+    WHERE wposts.ID = wpostmeta.post_id
+    AND (wpostmeta.meta_key = 'location' AND (wpostmeta.meta_value LIKE '%,".$roomId.",%' OR wpostmeta.meta_value LIKE '".$roomId.",%' OR wpostmeta.meta_value LIKE '%,".$roomId."' OR wpostmeta.meta_value LIKE '".$roomId."'))
+    AND (wpostmeta.meta_key = 'lastUpdate' AND wpostmeta.meta_value < ".$fifteen.")
+    AND wposts.post_type = 'device'
+    AND wposts.post_status = 'publish'
+    ";
+    $count15 = count($wpdb->get_results($querystr));
+
+    $querystr = "
+    SELECT wposts.*
+    FROM $wpdb->posts wposts, $wpdb->postmeta wpostmeta
+
+    WHERE wposts.ID = wpostmeta.post_id
+    AND (wpostmeta.meta_key = 'location' AND (wpostmeta.meta_value LIKE '%,".$roomId.",%' OR wpostmeta.meta_value LIKE '".$roomId.",%' OR wpostmeta.meta_value LIKE '%,".$roomId."' OR wpostmeta.meta_value LIKE '".$roomId."'))
+    AND (wpostmeta.meta_key = 'lastUpdate' AND wpostmeta.meta_value < ".$thirty.")
+    AND wposts.post_type = 'device'
+    AND wposts.post_status = 'publish'
+    ";
+    $count30 = count($wpdb->get_results($querystr));
+
+    $querystr = "
+    SELECT wposts.*
+    FROM $wpdb->posts wposts, $wpdb->postmeta wpostmeta
+
+    WHERE wposts.ID = wpostmeta.post_id
+    AND (wpostmeta.meta_key = 'location' AND (wpostmeta.meta_value LIKE '%,".$roomId.",%' OR wpostmeta.meta_value LIKE '".$roomId.",%' OR wpostmeta.meta_value LIKE '%,".$roomId."' OR wpostmeta.meta_value LIKE '".$roomId."'))
+    AND (wpostmeta.meta_key = 'lastUpdate' AND wpostmeta.meta_value < ".$fortyfive.")
+    AND wposts.post_type = 'device'
+    AND wposts.post_status = 'publish'
+    ";
+    $count45 = count($wpdb->get_results($querystr));
+
+    $querystr = "
+    SELECT wposts.*
+    FROM $wpdb->posts wposts, $wpdb->postmeta wpostmeta
+
+    WHERE wposts.ID = wpostmeta.post_id
+    AND (wpostmeta.meta_key = 'location' AND (wpostmeta.meta_value LIKE '%,".$roomId.",%' OR wpostmeta.meta_value LIKE '".$roomId.",%' OR wpostmeta.meta_value LIKE '%,".$roomId."' OR wpostmeta.meta_value LIKE '".$roomId."'))
+    AND (wpostmeta.meta_key = 'lastUpdate' AND wpostmeta.meta_value < ".$hour.")
+    AND wposts.post_type = 'device'
+    AND wposts.post_status = 'publish'
+    ";
+    $count60 = count($wpdb->get_results($querystr));
+
+    return array('all'=>$allCount,'15'=>$count15,'30'=>$count30,'45'=>$count45,'60'=>$count60);
 }
 
 $roomPosts = get_posts(array(
